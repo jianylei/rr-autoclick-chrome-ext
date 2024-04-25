391d19dd335bdc2b172d6ef1b0a958471a867256
Jian Yong Lei <jianyonglei@gmail.com>
2024-04-24 19:08:47 -0600

    fixed timer issue

--- public/content.js
+++ public/content.js
@@ -2,19 +2,21 @@ let isClickEnabled = true;
 
 function clickButton() {
     if (!isClickEnabled) return;
-
+    
     const iframe = document.querySelector('#cardashboardframe')
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document
     const iframe2 = iframeDoc.querySelector('#leftpaneframe')
     const iframeDoc2 = iframe2.contentDocument || iframe2.contentWindow.document
     const button = iframeDoc2.querySelector('input[type="button"][value="Claim"]')
     
+    console.log(button)
+
     if (button) {
         button.click();
     }
 }
 
-document.addEventListener('DOMContentLoaded', setTimeout(clickButton, 4000));
+document.addEventListener('DOMContentLoaded', () => setTimeout(clickButton, 4000));
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
